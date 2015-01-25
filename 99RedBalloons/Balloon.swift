b68781f5d5c0517ea647ef3a645e12d11a91e047
Adam Marx <houdini.marx@gmail.com>
2015-01-25 09:27:29 -0500

    first commit

---
+++ 99RedBalloons/Balloon.swift
@@ -0,0 +1,15 @@
+//
+//  Balloon.swift
+//  99RedBalloons
+//
+//  Created by Adam Marx on 1/24/15.
+//  Copyright (c) 2015 Adam Marx. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Balloon {
+    var number : Int
+    var image = UIImage(named:"")
+}
--- 99RedBalloons/ViewController.swift
+++ 99RedBalloons/ViewController.swift
@@ -10,9 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var NumberLabel: UILabel!
+    @IBOutlet weak var NextButton: UIBarButtonItem!
+    
+    var redBalloons : [Balloon] = []
+    
+    func createBalloons() {
+        var imageToSet = UIImage(named:"")
+        
+        for var i = 0; i < 99; i++ {
+            var randomNum = Int(arc4random_uniform(4))
+            switch randomNum {
+            case 0:
+                imageToSet = UIImage(named: "RedBalloon1.jpg")
+            case 1:
+                imageToSet = UIImage(named: "RedBalloon2.jpg")
+            case 2:
+                imageToSet = UIImage(named: "RedBalloon3.jpg")
+            default:
+                imageToSet = UIImage(named: "RedBalloon4.jpg")
+            }
+            
+            var newBalloon = Balloon(number: i, image: imageToSet)
+            redBalloons.append(newBalloon)
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        createBalloons()
+        NumberLabel.text = "99 Balloons!"
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +51,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func NextButtonPressed(sender: UIBarButtonItem) {
+            NumberLabel.text = "Whoa!"
+            var randomNum = Int(arc4random_uniform(99))
+            ImageView.image = redBalloons[randomNum].image
+            NumberLabel.text = String(redBalloons[randomNum].number + 1)
+    }
 
 }
 
